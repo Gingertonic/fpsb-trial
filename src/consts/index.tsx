341716ef23b3e45d5a2c75aad34b482e1eea2394
Gingertonic <beth@getfutureproof.co.uk>
2022-01-11 10:40:29 +0000

    add useful const links

---
+++ src/consts/index.tsx
@@ -0,0 +1,8 @@
+export const S3_PUBLIC: string = 'https://futureproof-public-documents.s3.eu-west-2.amazonaws.com';
+export const ICON: string = `${S3_PUBLIC}/brand-assets/favicon.ico`;
+export const DEVICE: string = `${S3_PUBLIC}/brand-assets/device.png`;
+export const LOGO: string = `${S3_PUBLIC}/brand-assets/logo.png`;
+export const WWW: string = "https://getfutureproof.co.uk";
+export const PLACEHOLDER: string = "https://upload.wikimedia.org/wikipedia/commons/a/ad/Placeholder_no_text.svg";
+export const DEMO_AVATAR: string = `${S3_PUBLIC}/lytical/avatar.jpeg`;
+export const DEMO_HEADSHOT: string = `${S3_PUBLIC}/hamilton/headshots/Pearl_Hamilton.png`;
